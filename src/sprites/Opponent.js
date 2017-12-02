40ab012a20e63e3e17a76a0b0b459ce926e7ebde
inspirsea <sunesson.emil@gmail.com>
2017-12-02 11:35:06 +0100

    First opponent push

---
+++ src/sprites/Opponent.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Sprite {
+  constructor ({ game, x, y, asset }) {
+    super(game, x, y, asset);
+    this.anchor.setTo(0.5);
+    this.goal = {
+      x: 400,
+      y: 100
+    };
+  }
+
+  update () {
+    this.accelerateTo(this.goal, 60);
+  }
+
+  accelerateTo(goal, speed) {
+    if (typeof speed === 'undefined') { speed = 60; }
+    var angle = Math.atan2(goal.y - this.y, goal.x - this.x);
+    this.body.rotation = angle + game.math.degToRad(90);
+    this.body.force.x = Math.cos(angle) * speed;
+    this.body.force.y = Math.sin(angle) * speed;
+  }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,12 +1,15 @@
 /* globals __DEV__ */
-import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Phaser from 'phaser';
+import Mushroom from '../sprites/Mushroom';
+import Opponent from '../sprites/Opponent';
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
 
-  create () {
+  init() { }
+  preload() { }
+
+  create() {
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
     const bannerText = 'Phaser + ES6 + Webpack'
     let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
     banner.font = 'Bangers'
@@ -21,14 +24,35 @@ export default class extends Phaser.State {
       x: this.world.centerX,
       y: this.world.centerY,
       asset: 'mushroom'
-    })
+    });
 
-    this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.mushroom);
+
+    this.createOpponents();
   }
 
-  render () {
+  render() {
     if (__DEV__) {
       this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
+  }
+
+  createOpponents() {
+    var rect = game.make.bitmapData(32, 32);
+    rect.ctx.fillStyle = '#aa0000';
+    rect.ctx.fillRect(0, 0, 32, 32);
+
+    this.opponents = this.game.add.group();
+
+    let opponent = new Opponent({
+      game: this.game,
+      x: 100,
+      y: 100,
+      asset: rect
+    });
+
+    this.game.physics.p2.enable(opponent, false);
+
+    this.game.add.existing(opponent);
   }
 }
